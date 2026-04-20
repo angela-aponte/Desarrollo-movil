454399ccfc0fa5ab0a5848157a7bf05af3df9b0b
Angela <angela.aponte01@uceva.edu.co>
2026-04-20 11:39:28 -0500

    feat: Implementación de ejemplo de http

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -102,6 +102,14 @@ class CustomDrawer extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
+          ListTile(
+            leading: const Icon(Icons.memory),
+            title: const Text('Isolate'),
+            onTap: () {
+              context.go('/pokemons');
+              Navigator.pop(context);
+            },
+          ),
         ],
       ),
     );
